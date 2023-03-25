a2e73dc9240d2bb1d744fabefa3f56ac39a658a2
Albert Portnoy <albert@albertp.dev>
2023-03-25 18:02:20 -0500

    welcome disableable

--- src/modules/welcome.ts
+++ src/modules/welcome.ts
@@ -2,9 +2,11 @@ import { CommandClient, Member, Guild, GuildTextableChannel } from 'eris';
 import { IDS, MESSAGES } from '../constants.js';
 
 function processMember(guild: Guild, member: Member): void {
-  if(guild.id !== IDS.server) return;
+  if (guild.id !== IDS.server) return;
+  if (!IDS.channels.welcome) return;
     
   const welcomeChannel = guild.channels.get(IDS.channels.welcome) as GuildTextableChannel;
+  if (!welcomeChannel) return;
 
   welcomeChannel.createMessage(MESSAGES.welcome
     .replace(/%user/gi, `<@${member.id}>`)
